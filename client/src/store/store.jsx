1c911744a0cfc99777f8643abbd2f98c23817801
Александр Хорохорин <aleksandrhorohorin@MacBook-Air-Aleksandr.local>
2023-07-13 01:55:04 +0300

    update

--- client/src/store/store.jsx
+++ client/src/store/store.jsx
@@ -28,6 +28,7 @@ export default class Store{
             this.setAuth(true);
             this.setUser(response.data.username);
         } catch (error) {
+            console.log(error);
             console.log(error?.response?.data?.message);
         }
     }
