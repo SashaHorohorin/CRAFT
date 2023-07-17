daac3b80c04c1512ef564f7a2f4f05f2d3761911
Александр Хорохорин <aleksandrhorohorin@MacBook-Air-Aleksandr.local>
2023-07-17 17:29:30 +0300

    create workout card

--- client/src/components/Workouts/Workouts.jsx
+++ client/src/components/Workouts/Workouts.jsx
@@ -1,38 +1,75 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
-const Workouts = ({workouts, date}) => {
+const Workouts = ({ workouts, date }) => {
     const workLog = () => {
         console.log(workouts[0]?.type);
-    }
-  return (
-    <div className='workouts'>
-        <div onClick={() => workLog()} className="workouts__date date-workouts">
-            <div className="date-workouts__week">ПН</div>
-            <div className="date-workouts__day">{`${date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()}.${date.getMonth() < 10 ? `0${date.getMonth()}` : date.getMonth()}`}</div>
-        </div>
-        <div className="workouts__items items-workouts">
-            {
+    };
+    return (
+        <div className="workouts">
+            <div
+                onClick={() => workLog()}
+                className="workouts__date date-workouts"
+            >
+                <div className="date-workouts__week">ПН</div>
+                <div className="date-workouts__day">{`${
+                    date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
+                }.${
+                    date.getMonth() < 10
+                        ? `0${date.getMonth()}`
+                        : date.getMonth()
+                }`}</div>
+            </div>
+            <div className="workouts__items">
+                {/* {
                 workouts.map((workout, index) => (
                     <div>{workout?.id}</div>
                 ))
-            }
-            {/* <div className="workouts__item item-workouts">
-                <div className="item-workouts__row">
-                    <div className="item-workouts__column">
-                        <div className="item-workouts__name">Тренировка для начинающих и продолжающих</div>
-                        <div className="item-workouts__btn btn-workout">подробнее</div>
-                        
+            } */}
+                <div className="workouts__item workout">
+                    <div className="workout__row">
+                        <div className="workout__column">
+                            <div className="workout__info info-workout">
+                                <div className="info-workout__name">
+                                    Тренировка для начинающих и продолжающих
+                                </div>
+                                <div className="info-workout__btn btn-workout">
+                                    подробнее
+                                </div>
+                            </div>
+                        </div>
+                        <div className="workout__column">
+                            <div className="workout__time time-workout">
+                                <div className="time-workout__label">Время</div>
+                                <div className="time-workout__interval">18:30 - 20:00</div>
+                            </div>
+                        </div>
+                        <div className="workout__column">
+                            <div className="workout__trainer trainer-workout">
+                                <div className="trainer-workout__label">Тренер</div>
+                                <div className="trainer-workout__name">Хорохорин Александр</div>
+                            </div>
+                        </div>
+                        <div className="workout__column">
+                            <div className="workout__actions actions-workout">
+                                <div className="actions-workout__people people-actions">
+                                    <div className="people-actions__circles">
+                                        <div className="people-actions__circle"></div>
+                                        <div className="people-actions__circle"></div>
+                                        <div className="people-actions__circle"></div>
+                                    </div>
+                                    <div className="people-actions__count count-people">
+                                        <div className="count-people__title">и еще 7 человек</div>
+                                        <div className="count-people__text">пойдут на тренировку</div>
+                                    </div>
+                                </div>
+                                <div className="actions-workout__btn btn-workout">Записаться</div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="item-workouts__column"></div>
-                    <div className="item-workouts__column"></div>
-                    
                 </div>
-                
-            </div> */}
+            </div>
         </div>
-        
-    </div>
-  )
-}
+    );
+};
 
-export default Workouts
+export default Workouts;
