6e96c5b238c8407369a1a7fef10c67b557e88a3e
NikitaPie <nikita-pirogov-artur@mail.ru>
2023-08-07 01:34:42 +0300

    add findUserDto

--- server/src/main/java/com/craft/craft/controller/BaseUserController.java
+++ server/src/main/java/com/craft/craft/controller/BaseUserController.java
@@ -1,11 +1,15 @@
 package com.craft.craft.controller;
 
+import com.craft.craft.dto.FindUserDto;
 import com.craft.craft.dto.SetLabIdDto;
 import com.craft.craft.dto.sport.ProfileBaseUserResponseDto;
 import com.craft.craft.error.exeption.ModelNotFoundException;
 import com.craft.craft.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/profile")
@@ -28,5 +32,10 @@ public class BaseUserController {
     public Boolean setLAbId(@RequestBody SetLabIdDto dto) throws ModelNotFoundException {
         userService.setLabId(dto);
         return true;
+    }
+
+    @GetMapping("/find-all")
+    public List<FindUserDto> getAll(){
+        return userService.findAll().stream().map(FindUserDto::getDtoFromUser).collect(Collectors.toList());
     }
 }
