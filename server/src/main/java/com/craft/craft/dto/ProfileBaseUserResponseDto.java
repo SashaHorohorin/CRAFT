202a1857a5a22f5f7a4623a4675110b703ebdb17
NikitaPie <nikita-pirogov-artur@mail.ru>
2023-08-09 03:13:39 +0300

    update dto

--- server/src/main/java/com/craft/craft/dto/ProfileBaseUserResponseDto.java
+++ server/src/main/java/com/craft/craft/dto/ProfileBaseUserResponseDto.java
@@ -1,15 +1,15 @@
 package com.craft.craft.dto;
 
-import com.craft.craft.dto.sport.SportsmenDto;
 import com.craft.craft.dto.sport.competiton.CompetitionDto;
 import com.craft.craft.dto.sport.competiton.CompetitionPairDto;
-import com.craft.craft.dto.sport.competiton.CompetitionRequestsInProfileDto;
+import com.craft.craft.dto.sport.competiton.CompetitionRequestInProfileDto;
 import com.craft.craft.dto.sport.train.TrainInProfileResponseDto;
 import com.craft.craft.model.user.BaseUser;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,12 +24,32 @@ public class ProfileBaseUserResponseDto {
     protected String username;
     private List<TrainInProfileResponseDto> trains;
     private List<CompetitionDto> competitions;
-    private List<CompetitionRequestsInProfileDto> requestToInvite;
-    private List<CompetitionRequestsInProfileDto> requestToJoin;
+    private List<CompetitionRequestInProfileDto> requestToInvite;
+    private List<CompetitionRequestInProfileDto> requestToJoin;
     private Integer rating;
     private Integer labId;
 
-    public static ProfileBaseUserResponseDto getDtoFromBaseUser(BaseUser user){
+    public static ProfileBaseUserResponseDto getDtoFromBaseUser(BaseUser user) {
+        List<CompetitionRequestInProfileDto> invite = new ArrayList<>();
+        List<CompetitionRequestInProfileDto> join = new ArrayList();
+        user.getRequestToInviteCompetition().forEach(r -> {
+                    BaseUser u = r.getPlayers().iterator().next();
+                    invite.add(CompetitionRequestInProfileDto.getRequestDto(
+                            r.getId(),
+                            u,
+                            r.getCompetition()
+                    ));
+                }
+        );
+        user.getRequestToJoinCompetition().forEach(r -> {
+                    BaseUser u = r.getPlayers().iterator().next();
+                    join.add(CompetitionRequestInProfileDto.getRequestDto(
+                            r.getId(),
+                            u,
+                            r.getCompetition()
+                    ));
+                }
+        );
         return new ProfileBaseUserResponseDto(
                 user.getFirstName(),
                 user.getLastName(),
@@ -38,10 +58,10 @@ public class ProfileBaseUserResponseDto {
                 user.getUsername(),
                 user.getTrains().stream().map(TrainInProfileResponseDto::getDtoFromTrain).collect(Collectors.toList()),
                 user.getCompetitionPairs().stream().map(pair ->
-                     CompetitionDto.getDtoFromCompetition(pair.getCompetition())
+                        CompetitionDto.getDtoFromCompetition(pair.getCompetition())
                 ).collect(Collectors.toList()),
-                user.getRequestToInviteCompetition().stream().map(CompetitionRequestsInProfileDto::getDtoFromCompetitionPair).collect(Collectors.toList()),
-                user.getRequestToJoinCompetition().stream().map(CompetitionRequestsInProfileDto::getDtoFromCompetitionPair).collect(Collectors.toList()),
+                invite,
+                join,
                 user.getRating(),
                 user.getLabId()
         );
---
+++ server/src/main/java/com/craft/craft/dto/sport/competiton/CompetitionRequestInProfileDto.java
@@ -0,0 +1,43 @@
+package com.craft.craft.dto.sport.competiton;
+
+import com.craft.craft.dto.sport.SportsmenDto;
+import com.craft.craft.model.sport.Competition;
+import com.craft.craft.model.sport.CompetitionType;
+import com.craft.craft.model.sport.SportComplex;
+import com.craft.craft.model.user.BaseUser;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CompetitionRequestInProfileDto {
+
+    private UUID pairId;
+    private SportsmenDto request;
+    private UUID competitionId;
+    private SportComplex sportComplex;
+    private CompetitionType type;
+    private Date startCompetition;
+    private int maxParticipant;
+    private int nowParticipant;
+
+    public static CompetitionRequestInProfileDto getRequestDto(UUID id, BaseUser user, Competition competition) {
+        return new CompetitionRequestInProfileDto(
+                id,
+                SportsmenDto.getDtoFromBaseUser(user),
+                competition.getId(),
+                competition.getSportComplex(),
+                competition.getType(),
+                competition.getStartCompetition(),
+                competition.getMaxPair(),
+                competition.getNowPair()
+
+        );
+    }
+
+}
