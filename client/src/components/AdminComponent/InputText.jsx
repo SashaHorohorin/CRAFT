1529a6cedbcea2c4d5e1b55631748a199278bf1c
Александр Хорохорин <aleksandrhorohorin@MacBook-Air-Aleksandr.local>
2023-08-28 03:25:10 +0300

    delete file images

--- client/src/components/AdminComponent/InputText.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const InputText = ({handleFunction, ...arg}) => {
-  return (
-    <input {...arg} onChange={(e) => handleFunction(e)}/>
-  )
-}
-
-export default InputText
